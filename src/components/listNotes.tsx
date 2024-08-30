a482538f1bf86ab82fa3830b6e70b81e8b7573fa
Damilola Oyeyipo <damilola@7am.io>
2024-08-30 16:43:29 +0100

    delete notes array and update Notecard component

--- src/components/listNotes.tsx
+++ src/components/listNotes.tsx
@@ -1,31 +1,17 @@
 import { ListNotes, Note } from "@/interfaces/note.interface";
+import NoteCard from "./noteCard";
 
 export default function List(props: ListNotes) {
   const { notes } = props;
-  
-  
-    
-    if (!notes) return <div>No notes</div>
-    
+
+  if (!notes) return <div>No notes</div>;
+
   return (
     <div>
       {notes.map((note: Note) => {
         return (
           <div key={note._id}>
-            <div className="flex flex-col my-4 bg-slate-300 rounded-xl">
-              <div className="flex justify-between p-4 gap-40">
-                <div className="">
-                  <h2 className="text-neutral-950">Title : {note.title}</h2>
-                  <p className="text-neutral-950">Content : {note.content}</p>
-                </div>
-
-                <div className="flex justify-between gap-6">
-                  <button className="text-blue-500">Edit</button>
-                  <button className="text-red-500">Delete</button>
-                </div>
-              </div>
-            </div>
-            {/* <NoteCard title={note.title} content={note.content} /> */}
+            <NoteCard title={note.title} content={note.content} />
           </div>
         );
       })}
--- src/components/noteCard.tsx
+++ src/components/noteCard.tsx
@@ -3,9 +3,18 @@ import { Note } from "@/interfaces/note.interface";
 export default function NoteCard(props: Partial<Note>) {
   const { title, content } = props;
   return (
-    <div className="rounded-md p-10 bg-slate-500">
-      <h2 className="text-neutral-950">{title}</h2>
-      <p className="text-neutral-950">{content}</p>
+    <div className="flex flex-col my-4 bg-slate-300 rounded-xl">
+      <div className="flex justify-between p-4 gap-40">
+        <div className="">
+          <h2 className="text-neutral-950">Title : {title}</h2>
+          <p className="text-neutral-950">Content : {content}</p>
+        </div>
+
+        <div className="flex justify-between gap-6">
+          <button className="text-blue-500">Edit</button>
+          <button className="text-red-500">Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
