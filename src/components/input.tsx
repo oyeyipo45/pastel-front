dde3f378ca47d058b5d211a2e206291e9a851052
Damilola Oyeyipo <damilola@7am.io>
2024-08-30 18:39:57 +0100

    Add styling to note input compontent

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -2,16 +2,17 @@ import { useCreateNoteMutation } from "@/redux/api";
 import { useState } from "react";
 
 const NoteInput = () => {
-  const [title, SetTitle] = useState("");
-  const [content, SetContent] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [error, setError] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-
+    setError("");
     if (name === "title") {
-      SetTitle(value);
+      setTitle(value);
     } else if (name === "content") {
-      SetContent(value);
+      setContent(value);
     }
   };
 
@@ -19,21 +20,35 @@ const NoteInput = () => {
 
   const createPostHandler = async () => {
     if (!title || !content) {
-      alert("Please enter a fill all fields");
+      setError("Please fill all fields");
     } else {
       await createNote({ title, content }).unwrap();
-      SetTitle("");
-      SetContent("");
+      setError("");
+      setTitle("");
+      setContent("");
     }
   };
 
+  const errorVisibility = error ? "block" : "hidden";
+
   return (
-    <div className="postInput">
-      <input className="text-neutral-950" name="title" value={title} type="text" placeholder="Add Title" onChange={handleChange} />
-      <input className="text-neutral-950" name="content" value={content} type="text" placeholder="Add Content" onChange={handleChange} />
-      <button id="push" type="submit" onClick={createPostHandler}>
-        Add Post
-      </button>
+    <div className="">
+      <p className="text-neutral-950">Create Note</p>
+      <div className="flex justify-between my-4 gap-4">
+        <input className="text-neutral-950 p-2 rounded-md" name="title" value={title} type="text" placeholder="Add Title" onChange={handleChange} />
+        <input
+          className="text-neutral-950 p-2 rounded-md"
+          name="content"
+          value={content}
+          type="text"
+          placeholder="Add Content"
+          onChange={handleChange}
+        />
+        <button id="push" type="submit" onClick={createPostHandler} className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+          Add Post
+        </button>
+      </div>
+      <p className="text-red-500 h-[30px]">{error}</p>
     </div>
   );
 };
